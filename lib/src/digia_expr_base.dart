a78fd8336c84b3a75f9f22fa633710e0848bcc88
RamSuthar-Digia <ramsuthar@digia.tech>
2025-10-22 20:31:36 +0530

    added expression analyser

--- lib/src/digia_expr_base.dart
+++ lib/src/digia_expr_base.dart
@@ -2,5 +2,6 @@ export 'ast.dart';
 export 'base_expr_context.dart';
 export 'expr_context.dart';
 export 'expression.dart';
+export 'expression_analyzer.dart';
 export 'std/std_functions.dart';
 export 'types.dart';
